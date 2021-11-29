d53d5279da2e37a14494a6f6fd31146b94f8b0d3
Ekaterina Zhevnerova <ekaterinazhevnerova@gmail.com>
2021-11-29 23:38:55 +0300

    feat: home task done

--- tasks/dict_tasks/planet.py
+++ tasks/dict_tasks/planet.py
@@ -22,7 +22,17 @@ get_planet_name(3) -> 'Земля'
 
 
 def get_planet_name(planet_num: int) -> str:
-    result = None
+    PLANETS = {
+        1: 'Меркурий',
+        2: 'Венера',
+        3: 'Земля',
+        4: 'Марс',
+        5: 'Юпитер',
+        6: 'Сатурн',
+        7: 'Уран',
+        8: 'Нептун',
+    }
+    result = PLANETS.get(planet_num, None)
     return result
 
 
